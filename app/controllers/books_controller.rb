5b9ee287ba88bccc7b8dd3e518100d3899d4a552
ymspb <masayayamamoto99@gmail.com>
2024-11-14 15:12:44 +0900

    [Add] new, index, show, edit, update and destroy actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,21 +10,36 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
 
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Bookd was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
   end
 
   private
